fbdc3b05847663a85273ba1c6992e7a1add8da2f
Breta01 <breta.hajek@gmail.com>
2023-04-13 14:21:59 +0800

    Adding command line args

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-# python-flow
+# FELT Flow - FELT Labs command line too
 Running federated learning via python
+
+## Installation 
+First you have to install this python package:
+```
+pip install feltflow
+```
+
+Then you have to setup following environment variables:
+```bash
+export WEB3_INFURA_PROJECT_ID="a0b1..."
+export PRIVATE_KEY="0xa0b1..."
+```
+
+After that you should be ready for running FELT Flow.
+
+## Examples
+Example command line for running FELT Flow:
+```
+felt-flow --chain_id 80001 \
+   --name "Training name" \
+   --dids 'did:op:3632e8584837f2eac04d85466c0cebd8b8cb2673b472a82a310175da9730042a,did:op:cad4a81c9a8e1c1071ccf3e9dea6f8f42d58e100fa3ddf2950c8f0da9e0dda46' \
+   --algo_config '{"id":"FELT","name":"FELT Federated Training","assets":{"training":"did:op:87e58362dfc60bbeaf83d5495e587a891a9ca697a6c5ec3585bfe1f8586f85fa","aggregation":"did:op:dcefb784c302094251ae1bc19d898eb584bd7be20a623bab078d4df0283e6c79","emptyDataset":"did:op:20bf68f480e17aff3e6947792e75b615908a46394ba33c8cfb94587a0a8d2c29"},"hasParameters":true}' \
+   --algocustomdata '{"model_definition":{"model_name":"LinearRegression","model_type":"sklearn"},"data_type":"csv","target_column":-1}' \
+   --session <exported_session_cookie> \
+   --api_endpoint "https://app.feltlabs.ai"
+```
 
 
 ## Development
--- feltflow/training.py
+++ feltflow/training.py
@@ -1,8 +1,10 @@
+import argparse
+import json
 import logging
 import os
-import time
-
-from feltflow.cloud_storage import CloudStorage
+import sys
+from dataclasses import dataclass, field
+from typing import List, Optional, cast
 
 # Turn off info logging (must be before ocean_lib imports)
 logging.basicConfig(level=logging.ERROR)
@@ -10,6 +12,7 @@ logging.basicConfig(level=logging.ERROR)
 from brownie.network import accounts
 from dotenv import load_dotenv
 
+from feltflow.cloud_storage import CloudStorage
 from feltflow.config import get_ocean
 from feltflow.federated_training import FederatedTraining
 
@@ -20,57 +23,107 @@ load_dotenv()
 # os.environ["PRIVATE_KEY"] = ""
 
 
-def main():
-    # Create Ocean instance
-    ocean = get_ocean(80001)
-    storage = CloudStorage(
-        "http://localhost:3000",
-        os.getenv("SESSION"),  # TODO: Replace with call param
+@dataclass
+class Config:
+    name: str
+    chain_id: int
+    dids: List[str]
+    algo_config: dict
+    session: str
+    api_endpoint: str
+    algocustomdata: dict = field(default_factory=dict)
+
+
+def _help_exit(parser, error_msg=None):
+    """Print help of parser and quit the script."""
+    parser.print_help()
+    if error_msg:
+        print(f"\nERROR: {error_msg}")
+    sys.exit(2)
+
+
+def _parse_training_args(args_str: Optional[List[str]] = None) -> Config:
+    """Parse and partially validate arguments form command line.
+    Arguments are parsed from string args_str or command line if args_str is None
+
+    Args:
+        args_str: list with string arguments or None if using command line
+
+    Returns:
+        Parsed args object
+    """
+    parser = argparse.ArgumentParser(
+        prog="FELT Labs - Federated Learning",
+        description="""
+            Script for running federated learning with FELT Labs from command line.
+        """,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--name",
+        type=str,
+        required=True,
+        help="Name used for identifying the training.",
+    )
+    parser.add_argument(
+        "--chain_id",
+        type=int,
+        required=True,
+        help="Blockchain id identifying correct blockchain",
+    )
+    parser.add_argument(
+        "--dids",
+        type=lambda s: s.split(","),
+        required=True,
+        help="List of data DIDs split by comma (,)",
+    )
+    parser.add_argument(
+        "--algo_config",
+        type=lambda s: json.loads(s),
+        required=True,
+        help="Algorithm config passed as string representing JSON.",
+    )
+    parser.add_argument(
+        "--session",
+        type=str,
+        required=True,
+        help="User session cookie used for storing jobs in cloud storage",
+    )
+    parser.add_argument(
+        "--algocustomdata",
+        type=lambda s: json.loads(s),
+        default={},
+        help="Custom data which will be passed to algorithm during run (passed as string representing JSON).",
+    )
+    parser.add_argument(
+        "--api_endpoint",
+        type=str,
+        default="https://app.feltlabs.ai",
+        help="API endpoint URL for storing jobs data.",
     )
 
-    # Create Alice's wallet
+    args = parser.parse_args(args_str)
+    return cast(Config, args)
+
+
+def main(config: Optional[Config] = None, args_str: Optional[List[str]] = None) -> None:
+    if config is None:
+        config = _parse_training_args(args_str)
+
+    ocean = get_ocean(config.chain_id)
+    storage = CloudStorage(config.api_endpoint, config.session)
+
+    print("FELT Labs: Starting training")
     account = accounts.add(os.getenv("PRIVATE_KEY"))
-    print("Account balance", account.balance())
-    print("Ocean balance", ocean.OCEAN_token.balanceOf(account))
-
-    algocustomdata = {
-        "model_definition": {
-            "model_name": "LinearRegression",
-            "model_type": "sklearn",
-        },
-        "data_type": "csv",
-        "target_column": -1,
-    }
-
-    dids = [
-        "did:op:3632e8584837f2eac04d85466c0cebd8b8cb2673b472a82a310175da9730042a",
-        "did:op:cad4a81c9a8e1c1071ccf3e9dea6f8f42d58e100fa3ddf2950c8f0da9e0dda46",
-    ]
-
-    algo_config = {
-        "id": "FELT",
-        "name": "FELT Federated Training",
-        "assets": {
-            "training": "did:op:87e58362dfc60bbeaf83d5495e587a891a9ca697a6c5ec3585bfe1f8586f85fa",
-            "aggregation": "did:op:dcefb784c302094251ae1bc19d898eb584bd7be20a623bab078d4df0283e6c79",
-            "emptyDataset": "did:op:20bf68f480e17aff3e6947792e75b615908a46394ba33c8cfb94587a0a8d2c29",
-        },
-        "hasParameters": True,
-    }
+    print("Using account:", account.address)
+    print("    Account balance:", account.balance())
 
     federated_training = FederatedTraining(
-        ocean, storage, dids, algo_config, algocustomdata
+        ocean,
+        storage,
+        config.name,
+        config.dids,
+        config.algo_config,
+        config.algocustomdata,
     )
     federated_training.run(account, iterations=1)
-
-    ## TEST DIDs
-    # data_did = "did:op:3632e8584837f2eac04d85466c0cebd8b8cb2673b472a82a310175da9730042a"
-    # algo_did = "did:op:8d6f2b6689c1ae347aeeb4b2708c3db03bbb143e90671dfaa478c9b5b9a8af6a"
-
-    # compute_job = ComputeJob(ocean, [data_did], algo_did, algocustomdata)
-    # compute_job.start(account)
-    # while compute_job.state == "running":
-    #     compute_job.check_status(account)
-    #     time.sleep(5)
-
-    # print(compute_job.get_file_url("model", account))
